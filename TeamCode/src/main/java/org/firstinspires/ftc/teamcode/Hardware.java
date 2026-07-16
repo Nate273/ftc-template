9ddab0ad7096327b0e8da0699328e22810c6f924
Nate273 <nathan342isgreat@gmail.com>
2026-07-16 12:12:48 -0400

    Lots of code added to make the robot hopefully work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -5,19 +5,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hardware {
-    // 193.168.43.1
+    // 192.168.43.1
     public DcMotor left;
     public DcMotor right;
     public DcMotor claw;
     public Servo clearwater;
     public DcMotor rotater;
+    public Servo rotaterServo;
+    public Servo clawServo;
+    public Servo clearwaterFlorida;
     public double clawClose = .3;
     public double clawOpen = .7;
     public Hardware(HardwareMap hwMap){
         // Drive motors
         left = hwMap.get(DcMotor.class, "leftMotor");
         right = hwMap.get(DcMotor.class, "RightMotor");
-        rotater = hwMap.get(DcMotor.class, "da rotater");
+        rotater = hwMap.get(DcMotor.class, "rotater");
         claw = hwMap.get(DcMotor.class, "Claw");
 
         left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -34,8 +37,10 @@ public class Hardware {
         right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rotater.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         claw.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // servos and motors
+        // servo
         clearwater = hwMap.get(Servo.class, "clearwater");
+        rotaterServo = hwMap.get(Servo.class, "waterclear");
+        clawServo = hwMap.get(Servo.class, "clamper");
     }
 
     public void init(){
@@ -43,8 +48,10 @@ public class Hardware {
     }
     public void openClaw(){
         clearwater.setPosition(clawClose);
+        clawServo.setPosition(-clawClose);
     }
     public void closeCLaw(){
         clearwater.setPosition(clawOpen);
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -19,6 +19,9 @@ public class TeleOp extends LinearOpMode {
         robot = new Hardware(hardwareMap);
         robot.init();
         gamepad2.setTriggerThreshold(.5f);
+        robot.clearwater.setPosition(0);
+        robot.rotaterServo.setPosition(0);
+        robot.clearwaterFlorida.setPosition(0);
         // after this line is when the driver presses start
         waitForStart();
         // run until the end of the match (driver presses STOP)
@@ -32,31 +35,53 @@ public class TeleOp extends LinearOpMode {
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.right_stick_y;
-            double turn  =  gamepad1.left_stick_x;
+            double drive = -gamepad1.left_stick_x;
+            double turn  =  gamepad1.right_stick_y;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
             robot.left.setPower(leftPower);
             robot.right.setPower(rightPower);
 
-            if (gamepad2.square){
-                robot.openClaw();
-            }
-            if (gamepad2.triangle){
-                robot.closeCLaw();
-            }
-            if (gamepad2.right_bumper){
-                robot.claw.setPower(-0.5);
-            }
-            if (gamepad2.right_trigger_pressed){
-                robot.claw.setPower(0.5);
-            }
             if(gamepad1.right_trigger_pressed){
                 robot.rotater.setPower(0.158);
+            }else{
+                robot.rotater.setPower(0);
             }
             if (gamepad1.right_bumper){
                 robot.rotater.setPower(-0.158);
+            }else{
+                robot.rotater.setPower(0);
+            }
+
+            if (gamepad2.left_trigger_pressed){
+                robot.claw.setPower(-0.5);
+            }else{
+                robot.claw.setPower(0);
+            }
+            if (gamepad2.right_trigger_pressed){
+                robot.claw.setPower(0.5);
+            }else{
+                robot.claw.setPower(0);
+            }
+
+            if(gamepad2.left_bumper){
+                robot.rotaterServo.setPosition(1);
+            }else{
+                robot.rotaterServo.setPosition(0);
+            }
+            if (gamepad2.right_bumper){
+                robot.rotaterServo.setPosition(-1);
+            }else{
+                robot.rotaterServo.setPosition(0);
+            }
+
+            if (gamepad2.triangle){
+                robot.clearwater.setPosition(1);
+                robot.clawServo.setPosition(1);
+            }else{
+                robot.clearwater.setPosition(0);
+                robot.clawServo.setPosition(0);
             }
         }
     }
